51a767cf851948891b98883165ba66a54d2d79cb
Onaido Aliu Mutairu <onaidomutairu@gmail.com>
2022-07-02 07:06:15 -0700

    a program that prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,13 +14,13 @@ int main(void)
 
 	for (number = 0; number <= 9; number++)
 	{
-	
-		putchar(number + '0');								if (number < 9)
+	putchar(number + '0');								if (number < 9)
 	{	
-		putchar(',');									putchar(' ');
+	putchar(',');									putchar(' ');
 	}
 	}
 
-		putchar('\n');							return (0);
+	putchar('\n');
+return (0);
 
 }
