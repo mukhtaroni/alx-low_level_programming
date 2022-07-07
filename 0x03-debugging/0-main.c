20381fc4275d53c7e10f98481904aad8b705cef9
Onaido Aliu Mutairu <onaidomutairu@gmail.com>
2022-07-07 03:42:06 -0700

    causes an infinite loop

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,9 @@
 #include "main.h"
 
-
-
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
-
 
 int main(void)
 {
@@ -16,5 +13,4 @@ int main(void)
 	positive_or_negative(i);
 	
 	return (0);
-
 }
